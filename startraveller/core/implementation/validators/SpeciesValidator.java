98c85f3ed4b67cb63d46b722fec95bb1f3c2c9f1
Mike Pors <mike.pors@gmail.com>
2016-03-29 16:20:16 -0600

    Improves SpeciesValidator.
    SpeciesValidator now requires that the species chosen is from the list of
    main species.

--- startraveller/core/implementation/validators/SpeciesValidator.java
+++ startraveller/core/implementation/validators/SpeciesValidator.java
@@ -1,17 +1,35 @@
 package startraveller.core.implementation.validators;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import core.GameSystem;
 import core.PreCharacter;
 import startraveller.core.Validator;
 import startraveller.core.implementation.choices.SpeciesChoice;
 
+/*
+ * This validator does not handle custom species.
+ */
+
 public class SpeciesValidator implements Validator {
 	@Override
 	public boolean validateCharacter(PreCharacter precharacter) {
+		Set<String> knownSpecies = new HashSet<>(Arrays.asList(
+				"HUMAN",
+				"VESP",
+				"NYMPH",
+				"NIX",
+				"JOTUNN",
+				"ARCHON",
+				"HOMONCULUS",
+				"TSUKU"
+		));
 		try {
 			SpeciesChoice speciesChoice = (SpeciesChoice) precharacter.get("Species");
 			String species = speciesChoice.getSpecies();
-			return species != null && !species.isEmpty();
+			return species != null && !species.isEmpty() && knownSpecies.contains(species.trim().toUpperCase());
 		} catch (ClassCastException e) {
 			return false;
 		}
